7e9eecd3491cb7c5a64576588bf9b54e32a56869
Bohdan Chornopolskyi <bohdan.chornopolskyi@gmail.com>
2026-04-03 15:51:58 +0200

    feat: extend TypeScript types to include accounts, sessions, and workspaces in the database schema

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,5 +1,6 @@
 import { users } from "./users";
 import { sessions } from "./sessions";
 import { accounts } from "./accounts";
+import { workspaces } from "./workspaces";
 
-export { users, sessions, accounts };
+export { users, sessions, accounts, workspaces };
--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,7 +1,24 @@
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { users } from "./schema";
+import { accounts, sessions, users, workspaces } from "./schema";
 
 type UsersSelect = InferSelectModel<typeof users>;
 type UsersInsert = InferInsertModel<typeof users>;
 
-export { type UsersInsert, type UsersSelect };
+type AccountsSelect = InferSelectModel<typeof accounts>;
+type AccountsInsert = InferInsertModel<typeof accounts>;
+
+type WorkspacesSelect = InferSelectModel<typeof workspaces>;
+type WorkspacesInsert = InferInsertModel<typeof workspaces>;
+type SessionsSelect = InferSelectModel<typeof sessions>;
+type SessionsInsert = InferInsertModel<typeof sessions>;
+
+export {
+  type UsersInsert,
+  type UsersSelect,
+  type AccountsInsert,
+  type AccountsSelect,
+  type SessionsInsert,
+  type SessionsSelect,
+  type WorkspacesInsert,
+  type WorkspacesSelect,
+};
